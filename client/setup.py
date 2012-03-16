528438f026377c462e02ad460f9f444b7ec60015
Brian Waldon <bcwaldon@gmail.com>
2012-03-16 14:23:27 -0700

    Adding python client v0.0

---
+++ client/setup.py
@@ -0,0 +1,11 @@
+
+import distutils.core
+
+distutils.core.setup(
+    name="cloudtee",
+    version="0.0",
+    url="http://cloudtee.me",
+    author="Brian Waldon",
+    author_email="bcwaldon@gmail.com",
+    scripts=["bin/cloudtee"],
+)
